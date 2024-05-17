9070d978ae73345e0b257202b997b831b8bd7838
parakh2001 <parakh.gupta2571@gmail.com>
2024-05-17 11:45:18 +0530

    adding nvim configs

---
+++ lua/theprimeagen/packer.lua
@@ -0,0 +1,44 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use('nvim-treesitter/playground')
+	use({ 'rose-pine/neovim', as = 'rose-pine' })
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+	use{
+		'ThePrimeagen/harpoon',
+	}
+	use 'mbbill/undotree'
+	use 'tpope/vim-fugitive'
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+			{'neovim/nvim-lspconfig'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+    use { 'saadparwaiz1/cmp_luasnip' }
+	use "rafamadriz/friendly-snippets"
+    use {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    }
+end)
